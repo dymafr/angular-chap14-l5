600b0090689dcdcd95e7a1f93e016b665d47d2fc
dymafr <erwan1309@hotmail.com>
2021-01-05 14:00:30 +0100

    2 files modified

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class UserListComponent implements OnInit {
   @Input() names: string[];
+  @Input() search: string;
 
   constructor() {}
 
